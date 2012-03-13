94252ca12c5d48fa22665a6a3950c71296f89ee1
jonesbb <jonesbb@gmail.com>
2012-03-13 14:29:24 -0700

    added twilio test to app

--- app.rb
+++ app.rb
@@ -1,5 +1,24 @@
 require 'sinatra'
+require 'builder'
 
-get '/' do
+get '/index' do
       File.read(File.join('public', 'index.html'))
+end
+
+get '/hello' do
+  "Hello, world!"
+end
+
+get '/public/twilio_tides.rb' do
+      File.read(File.join('public', 'twilio_tides.rb'))
+end
+
+
+post '/' do
+  builder do |xml|
+    xml.instruct!
+    xml.Response do
+      xml.Sms("Hello from my Heroku app")
+    end
+  end
 end
--- startup.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env sh
-
-heroku create $1
-git push heroku master
